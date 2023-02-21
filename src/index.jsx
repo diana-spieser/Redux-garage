f111b9a79f3b1dbd8d67e72ff363796141036116
diana spieser <dianaspieser@gmail.com>
2023-02-21 16:03:49 +0100

    ok but dowgraded verssions of redct, redux, form to make it work

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { reducer as formReducer } from 'redux-form';
 
+import CarsIndex from './containers/cars_index';
+import CarsShow from './containers/cars_show';
+import CarsNew from './containers/cars_new';
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
+import carsReducer from './reducers/cars_reducer.js';
+
+const history = createBrowserHistory();
+
+// State and reducers
+const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
+const initialState = {
+  garage: garageName,
+  cars: []
 };
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
+const reducers = combineReducers({
+  garage: (state = null, action) => state,
+  cars: carsReducer,
+  form: formReducer
+});
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={createStore(reducers, initialState, middlewares)}>
+    <Router history={history}>
+      <div className="view-container">
+        <Switch>
+          <Route path="/" exact component={CarsIndex} />
+          <Route path="/cars/new" exact component={CarsNew} />
+          <Route path="/cars/:id" component={CarsShow} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
+  document.querySelector('.container')
+);
